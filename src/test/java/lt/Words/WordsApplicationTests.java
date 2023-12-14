378005b329ef769ad6d9bb4358c82d793964113c
Liutauras Butkinas <butkinas@gmail.com>
2023-12-14 22:37:22 +0200

    initial commit

---
+++ src/test/java/lt/Words/WordsApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.Words;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
